9e46dba29a19940882cf2b5a5fb8a0330c6774aa
Fyodor Wolf <fwolf@bu.edu>
2016-12-20 17:06:11 -0500

    working MEAN APP

---
+++ public/javascripts/angularConfig.js
@@ -0,0 +1,48 @@
+app.config([ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+	.state( 'home', { 
+		url: '/home', 		
+		templateUrl: '/home.html', 	
+		controller: 'MainCtrl', 
+		resolve: {
+			// resolve all promises before continuing.
+		    postPromise: ['posts', function(posts){
+		      	return posts.getAll();
+		    }],
+  		}
+  	})
+	.state( 'posts', { 
+		url: '/posts/{id}', 	
+		templateUrl: '/posts.html', 
+		controller: 'PostsCtrl', 
+		resolve: {
+			//inject post instance into posts controler
+			post: ['$stateParams', 'posts', function($stateParams, posts) {
+				return posts.get($stateParams.id);
+			}],
+		}
+	})
+	.state('login', {
+		url: '/login',
+		templateUrl: '/login.html',
+		controller: 'AuthCtrl',
+		onEnter: ['$state', 'auth', function($state, auth){
+			if(auth.isLoggedIn()){
+			  $state.go('home');
+			}
+		}]
+	})
+	.state('register', {
+		url: '/register',
+		templateUrl: '/register.html',
+		controller: 'AuthCtrl',
+		onEnter: ['$state', 'auth', function($state, auth){
+			if(auth.isLoggedIn()){
+			  $state.go('home');
+			}
+		}]
+	});
+
+	$urlRouterProvider.otherwise('home');
+
+}]);
